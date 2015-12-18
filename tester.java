0c8080b3ae5c049559811b1bcebf5f429b241dad
Steven O'Brien <steven.obrien@cix.csi.cuny.edu>
2015-12-18 07:43:49 -0500

    Create tester.java

---
+++ tester.java
@@ -0,0 +1,11 @@
+package edu.cuny.csi.csc330.spring15.VendingMachine;
+
+public class VendingMachineTester {
+	
+	public static void main(String [] args ){
+		
+		VendingMachine test = new VendingMachine();
+		
+	}
+
+}
